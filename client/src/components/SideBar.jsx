7c1f98883661903b63f23b1cef02595cbc5c5794
asrith-raju <asrithraju143@gmail.com>
2026-02-10 10:19:43 +0530

    client(feat): add new chat button and search functionality to SideBar component

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -9,6 +9,18 @@ const SideBar = () => {
     <div className='flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b- from-[#242124] to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1'>
       {/* logo */}
       <img src={theme ==='dark'?assets.logo_full:assets.logo_full_dark} alt="" className='w-full max-w-48' />
+
+       {/* new chat button */}
+       <button className='flex justify-center items-center w-full py-2 mt-10 text-white bg-gradient-to-r from-[#A456F7] to-[#3D81F6] text-sm rounded-md cursor-pointer'>
+        <span className='mr-2 text-xl'>+</span>New Chat
+       </button>
+
+       {/* search conversations */}
+
+       <div className='flex items-center gap-2 p-3 mt-4 border border-gray-400 dark:border-white/20 rounded-md'>
+           <img src={assets.search_icon} alt="" className='w-4 not-dark:invert'/>
+           <input onChange={(e)=>setSearch(e.target.value) } value={search} type="text" placeholder='Search conversations' className='text-xs placeholder:text-gray-400 outline-none'/>
+       </div>
     </div>
   ) 
 }
