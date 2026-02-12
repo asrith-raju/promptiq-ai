9cbcc7f3cc299eb1deafc8744335c2c9dac5a049
asrith-raju <asrithraju143@gmail.com>
2026-02-12 22:03:58 +0530

    client(feat): add dark mode toggle button

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -66,11 +66,18 @@ const SideBar = () => {
       </div>
 
       {/* Dark Mode Toggle */}
-      <div onClick={()=>{navigate('/community')}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15rounded-md  '>
+      <div onClick={()=>{navigate('/community')}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md  justify-between cursor-pointer '>
         <div className='flex items-center gap-2 text-sm'>
           <img src={assets.theme_icon} className='w-4 not-dark:invert' alt="" />
           <p>Dark Mode</p>
         </div>
+        <label className='relative inline-flex cursor-pointer'>
+          <input onClick={()=>setTheme(theme === 'dark' ? 'light' :'dark')} type="checkbox" className='sr-only peer' checked={theme==='dark'}/>
+          <div className='w-9 h-5 bg-gray-400 rounded-full peer-checked:bg-purple-600 transition-all'>
+
+          </div>
+          <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4'></span>
+        </label>
       </div>
     </div>
   )
