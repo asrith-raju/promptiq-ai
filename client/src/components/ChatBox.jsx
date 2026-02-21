68fd9bd4a6b4e432ab3a64d43c795fd2834643eb
asrith-raju <asrithraju143@gmail.com>
2026-02-21 20:03:11 +0530

    client(feat): add image publishing option in community

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,10 +1,10 @@
-import React, { use, useEffect, useState } from 'react'
+import React, { use, useEffect, useRef, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
 import Message from './Message'
 
 const ChatBox = () => {
-
+  const containerRef = useRef(null)
   const {selectedChat,theme} = useAppContext()
   const [messages,setMessages] = useState([])
   const [loading,setLoading] = useState(false)
@@ -20,10 +20,12 @@ const ChatBox = () => {
       setMessages(selectedChat.messages)
     }
     },[selectedChat])
+
+    
   return (
     <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
       {/* Chat Messages */}
-      <div className='flex-1 mb-5 overflow-y-scroll'>
+      <div ref={containerRef} className='flex-1 mb-5 overflow-y-scroll'>
          {messages?.length === 0 && (
           <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
             <img src={theme === 'dark' ? assets.logo_full: assets.logo_full_dark} className='w-full max-w-56 sm:max-w-68' alt="" />
@@ -43,6 +45,14 @@ const ChatBox = () => {
           </div>
          }
       </div>
+      {
+        mode==='image' && (
+          <label className='inline-flex items-center gap-2 mb-3 text-sm mx-auto'>
+            <p className='text-xs'>Publish Generated Image to Community</p>
+            <input type="checkbox" className='cursor-pointer' checked = {isPublished} onChange={(e)=>setIsPublished(e.target.checked)}/>
+          </label>
+        ) 
+      }
       {/* Prompt Input box */}
       <form onSubmit={onSubmit} className='bg-primary/20 dark:bg-[#583C97]/30 border border-primary dark:border-[#80609F]/30 rounded-full w-full max-w-2xl p-3 pl-4 mx-auto flex gap-4 items-center'>
          <select onChange={(e)=>setMode(e.target.value)} value={mode} className='text-sm pl-3 pr-2 outline-none'>
