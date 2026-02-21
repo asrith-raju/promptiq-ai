b012a28396581670c33e3a9f3ed047bc37623464
asrith-raju <asrithraju143@gmail.com>
2026-02-21 14:50:03 +0530

    client(fix): integrate Prism.js for syntax highlighting and enhance timestamp display in Message component

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { use, useEffect } from 'react'
 import { assets } from '../assets/assets'
 import moment from 'moment'
 import Markdown from 'react-markdown'
+import Prism from 'prismjs'
 
 const Message = ({message}) => {
+    
+  useEffect(() => {
+
+  Prism.highlightAll();
+  },[message.content])
   return (
     <div> 
           {message.role === "user" ? (
@@ -23,7 +29,7 @@ const Message = ({message}) => {
                   (
                     <div className='text-sm dark:text-primary reset-tw'> <Markdown>{message.content}</Markdown></div>
                   )}
-                  <span>{moment(message.timestamp).fromNow()}</span>
+                  <span className='text-xs text-gray-400 dark:text-[#B1A6C0]'>{moment(message.timestamp).fromNow()}</span>
             </div>
           )
           }
