b4d65043abc8d13f22e2485b7f1e26cd93ff8d30
asrith-raju <asrithraju143@gmail.com>
2026-02-21 20:03:44 +0530

    client(feat): add smooth scrolling to ChatBox on new messages

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -21,6 +21,14 @@ const ChatBox = () => {
     }
     },[selectedChat])
 
+    useEffect(() => {
+      if(containerRef.current){
+        containerRef.current.scrollTo({
+          top:containerRef.current.scrollHeight,
+          behavior: "smooth"
+        })
+      }
+    }, [messages])
     
   return (
     <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
@@ -29,7 +37,7 @@ const ChatBox = () => {
          {messages?.length === 0 && (
           <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
             <img src={theme === 'dark' ? assets.logo_full: assets.logo_full_dark} className='w-full max-w-56 sm:max-w-68' alt="" />
-            <p className='mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-wite'>Ask me anything</p>
+            <p className='mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-white'>Ask me anything</p>
           </div>
          )}
          {messages.map((message,index)=> <Message key={index} message={message}/>)}
